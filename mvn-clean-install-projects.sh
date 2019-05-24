86f22ef6e50853cbd9f0528b66ebea2bffbf8434
Christian Tischer <christian.tischer@embl.de>
2019-05-24 16:55:29 +0200

    Add build script

---
+++ mvn-clean-install-projects.sh
@@ -0,0 +1,7 @@
+cd ../imglib2-lazy-algorithm; mvn clean install
+cd ../bdv-utils; mvn clean install
+cd ../table-utils; mvn clean install
+cd ../imaris-writer; mvn clean install
+cd ../classifiers; mvn clean install
+cd ../fiji-plugin-platynereisViewer; mvn clean install
+cd ../pom-embl-cba
