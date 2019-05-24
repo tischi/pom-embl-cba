ae3f81768a62db99a3cda1cc6a761f57ed3ff43e
Christian Tischer <christian.tischer@embl.de>
2019-05-24 18:27:17 +0200

    Improve parent pom

--- mvn-clean-install-projects.sh
+++ mvn-clean-install-projects.sh
@@ -1,7 +1,8 @@
-cd ../imglib2-lazy-algorithm; mvn clean install
-cd ../bdv-utils; mvn clean install
-cd ../table-utils; mvn clean install
-cd ../imaris-writer; mvn clean install
-cd ../classifiers; mvn clean install
-cd ../fiji-plugin-platynereisViewer; mvn clean install
+cd ../imglib2-lazy-algorithm; mvn clean install deploy
+cd ../bdv-utils; mvn clean install deploy 
+cd ../table-utils; mvn clean install deploy
+cd ../imaris-writer; mvn clean install deploy
+cd ../classifiers; mvn clean install deploy
+cd ../fiji-plugin-plateViewer; mvn clean install deploy
+cd ../fiji-plugin-platynereisViewer; mvn clean install deploy
 cd ../pom-embl-cba
