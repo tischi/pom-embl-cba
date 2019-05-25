72c805fe45f7115f8f7df8829c8532f6f9c02d98
Christian Tischer <christian.tischer@embl.de>
2019-05-25 15:43:05 +0200

    Improve build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+cd ../imglib2-lazy-algorithm; mvn $@
+cd ../bdv-utils; mvn $@ 
+cd ../table-utils; mvn $@
+cd ../imaris-writer; mvn $@
+cd ../classifiers; mvn $@
+cd ../fiji-plugin-plateViewer; mvn $@
+cd ../fiji-plugin-platynereisViewer; mvn $@
+cd ../pom-embl-cba
--- mvn-clean-install-projects.sh
+++
@@ -1,8 +0,0 @@
-cd ../imglib2-lazy-algorithm; mvn clean install deploy
-cd ../bdv-utils; mvn clean install deploy 
-cd ../table-utils; mvn clean install deploy
-cd ../imaris-writer; mvn clean install deploy
-cd ../classifiers; mvn clean install deploy
-cd ../fiji-plugin-plateViewer; mvn clean install deploy
-cd ../fiji-plugin-platynereisViewer; mvn clean install deploy
-cd ../pom-embl-cba
