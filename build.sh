f7204f38f470051bd67ab631c7753722b186bada
Christian Tischer <christian.tischer@embl.de>
2019-05-31 17:44:30 +0200

    Add more dependencies

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ cd ../bdv-utils; mvn $@
 cd ../table-utils; mvn $@
 cd ../imaris-writer; mvn $@
 cd ../classifiers; mvn $@
+cd ../image-transform-converters; mvn $@
 cd ../fiji-plugin-plateViewer; mvn $@
 cd ../fiji-plugin-platynereisViewer; mvn $@
+cd ../fiji-plugin-morphometry; mvn $@
 cd ../pom-embl-cba
