0cb46abdf6231c6142a05c1e639c512172f121ae
Christian Tischer <christian.tischer@embl.de>
2019-06-06 17:41:02 +0200

    Add update pom script

--- build.sh
+++ build.sh
@@ -4,7 +4,13 @@ cd ../table-utils; mvn $@
 cd ../imaris-writer; mvn $@
 cd ../classifiers; mvn $@
 cd ../image-transform-converters; mvn $@
+cd ../transforms-utils; mvn $@
+cd ../fiji-slurm; mvn $@
+
 cd ../fiji-plugin-plateViewer; mvn $@
 cd ../fiji-plugin-platynereisViewer; mvn $@
 cd ../fiji-plugin-morphometry; mvn $@
+cd ../fiji-plugin-bigDataProcessor2; mvn $@
+cd ../fiji_plugin_bigDataProcessor; mvn $@
+
 cd ../pom-embl-cba
---
+++ update.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python /Users/tischer/Documents/pom_updater/update_poms.py --parent-path /Users/tischer/Documents/pom-embl-cba/pom.xml --children-dir /Users/tischer/Documents/ --no-backups
